ea03b3e0591806e2231ef55a92c366e3f5ad1506
Bradley Wascher <BradleyWascher@gmail.com>
2018-05-05 13:58:51 -0400

    Added First commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public Rigidbody rb;
+    public float moveSpeed = 50f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponentInParent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * moveSpeed);
+        }
+    }
+}
